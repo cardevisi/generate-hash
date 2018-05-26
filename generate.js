b0a210a967d12f2b8edb7d904127631842e6d1cd
Carlos Silveira <cardevisi@gmail.com>
2018-05-26 19:09:40 -0300

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,14 @@
+function toHex(input) {
+
+  var hash = "",
+    alphabet = "0123456789abcdef",
+    alphabetLength = alphabet.length;
+
+  do {
+    hash = alphabet[input % alphabetLength] + hash;
+    input = parseInt(input / alphabetLength, 10);
+  } while (input);
+
+  return hash;
+
+}
